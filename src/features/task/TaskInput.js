801db25f0ce91bd97fede49418fcd3ad5676d7ff
d-takeuchi <kuronk.kuro3@gmail.com>
2021-11-22 14:47:23 +0900

    sliceの追加等

---
+++ src/features/task/TaskInput.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { newTask } from "./taskSlice";
+
+const TaskInput = () => {
+  const dispatch = useDispatch();
+  const [editTitle, setEditTitle] = useState("");
+  const handleTitleChange = (e) => setEditTitle(e.target.value);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(newTask(editTitle));
+    setEditTitle("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        onChange={handleTitleChange}
+        value={editTitle}
+        placeholder="Please type in"
+      />
+      <button type="submit">NEW</button>
+    </form>
+  );
+};
+
+export default TaskInput;
